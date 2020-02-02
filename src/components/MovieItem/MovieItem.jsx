d6eb3358bdb7c7b93f81291c98ad1838e769c189
Tou Xiong <touyeexiong@gmail.com>
2020-02-02 15:35:55 -0600

    conlogging on click

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class MovieItem extends Component {
+
+    handleDes = (event) => {
+        console.log('clicked on poster!');
+
+    }
     render () {
+
         return (
             <>
                 <li key>
-                    <div><img src={this.props.movie.poster} alt={this.props.movie.title}/></div>
-                    <div>{this.props.movie.title}</div>
-                    <div>{this.props.movie.description}</div>
+                    <span><div><h2>{this.props.movie.title}</h2></div>
+                        <div>{this.props.movie.description}</div></span>
                     
+                    <span><img src={this.props.movie.poster} alt={this.props.movie.title} onClick={this.handleDes} /></span>
                     </li>
             
             </>
